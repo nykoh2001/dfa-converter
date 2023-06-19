e763b96a4b37cd9d23933b0254d0b14452b35adf
nykoh2001 <nykoh2001@gmail.com>
2023-06-19 19:57:51 +0900

    feat: recognize string

---
+++ recognize.py
@@ -0,0 +1,20 @@
+def recognize_string(DFA, string):
+    print(string)
+
+    final_state = DFA.final_state
+    start_state = DFA.start_state
+    delta_function = DFA.delta_funcs
+    current_state = start_state
+    # naming = DFA.naming
+    for c in string:
+        for d in delta_function:
+            print(d.state)
+            if current_state in d.state and c == d.symbol:
+                current_state = d.next
+                break
+
+    print("current, final", current_state, final_state)
+    if current_state in final_state:
+        return string + " 문자열은 인식되었습니다."
+    else:
+        return "인식할 수 없는 문자열입니다."
